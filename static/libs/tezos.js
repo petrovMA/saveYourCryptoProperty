b3a35d36e88ee0186e6d2db1c8743a098f6b105f
petrov <petrovma92@gmail.com>
2019-04-27 04:09:39 +0300

    receive property function!

--- static/libs/tezos.js
+++ static/libs/tezos.js
@@ -13,37 +13,12 @@ function loadData () {
 
 function pingContract () {
   // eztz.node.setDebugMode(true)
-
   eztz.node.setProvider('https://alphanet-node.tzscan.io')
-  // eztz.node.setProvider('http://209.250.227.9:8732')
-  // eztz.node.query(`/chains/main/blocks/head/context/contracts/KT1A5oZVuRWrejXmLgr7bcsa4fGc1b3WLn5V/big_map_get`, {
-  //   'key': {
-  //     'string': 'KT1A5oZVuRWrejXmLgr7bcsa4fGc1b3WLn5V'
-  //   },
-  //   'type': {
-  //     'prim': 'address'
-  //   }
-  // }).then(console.log).catch(console.error)
-
-  /*
-  eztz.node.query("/chains/main/blocks/head/context/contracts/KT1A5oZVuRWrejXmLgr7bcsa4fGc1b3WLn5V").then(function(res){
-    console.log(res);
-  }).catch(function(e){console.log(e)});
-
-  console.log('Opresult!!!') // Operation result*/
-  let from = 'tz1QLLbmqmrnfy7pBSzLCsY4RGSoB7t7Y72q'
 
   let mnem = 'course casual original captain luggage husband knock october balance learn floor phone wheat bike snake'
   let keys = eztz.crypto.generateKeys(mnem, 'iyuxlmea.qitsymvq@tezos.example.orgd0fe1QkS4V')
   let account = keys.pkh
 
-  // let addr = 'KT1A5oZVuRWrejXmLgr7bcsa4fGc1b3WLn5V'
-  // eztz.contract.send(addr, account, keys, 6, '(Left (Pair "test" 1))', 100000, 400000, 60000)
-
-  // let addr = 'tz1N5cvoGZFNYWBp2NbCWhaRXuLQf6e1gZrv'
-  // let addr = 'KT1A5oZVuRWrejXmLgr7bcsa4fGc1b3WLn5V' /* voting */
-  // {prim: "Right", args: [{prim: "Left", args: [{prim: "Unit"}]}]}   ---   update
-  // {prim: "Left", args: [{prim: "Pair", args: [{int: "0"}, {string: ""}]}]}
   let addr = 'KT1REci1iEvCLJYYigY3TupRvBugRmdWNWTv'
   eztz.contract.send(addr, account, keys, 6, '(Right (Left Unit))', 100000, 400000, 60000)
     .then(function (res) {
@@ -52,35 +27,23 @@ function pingContract () {
     }).catch(function (e) {
       console.log(e)
     })
-
-  /*
-  var operation = {
-    'kind': 'transaction',
-    'amount': 1000, // This is in mutez, i.e. 1000000 = 1.00 tez
-    'destination': 'tz1YWtADpgGehcTFqxMapiJcfLgzzL79aDJQ',
-    'fee': 100000
-  }
-  eztz.rpc.sendOperation(from, operation, keys).then(function (res) {
-    console.log(res) // Operation result
-  }).catch(function (e) {
-    console.log(e)
-  })
-
-  */
-
-/*  let e = 'KT1REci1iEvCLJYYigY3TupRvBugRmdWNWTv'
-  let r = 100 + ''
-  let n = '"Unit"'
-  let i = .5 + ''
-  // eztz.contract.send(contract, keys, amount, '(Left (Pair "test" 1))', fee, gasLimit, storageLimit)
-  eztz.contract.send().then(function (res) {
-    $('#balance').html(res)
-  }).catch(function (e) {
-    console.log(e)
-  })*/
 }
 
-/*
-vote address:
-KT1A5oZVuRWrejXmLgr7bcsa4fGc1b3WLn5V
-* */
+function receiveProperty () {
+  // eztz.node.setDebugMode(true)
+  eztz.node.setProvider('https://alphanet-node.tzscan.io')
+
+  let mnem = 'cruise pill rhythm enter remain flat sense traffic goat paper blossom super disagree mother speed'
+  let keys = eztz.crypto.generateKeys(mnem, 'gugmtvax.nuekgxlw@tezos.example.orgaEVLOPaRD3')
+  let account = keys.pkh
+
+  let addr = 'KT1REci1iEvCLJYYigY3TupRvBugRmdWNWTv'
+  // [{prim: "Right", args: [{prim: "Right", args: [{prim: "Left", args: [{prim: "Unit"}]}]}]}]
+  eztz.contract.send(addr, account, keys, 6, '(Right (Right (Right (Left Unit))))', 100000, 400000, 60000)
+    .then(function (res) {
+      console.log('Operation result!!!') // Operation result
+      console.log(res) // Operation result
+    }).catch(function (e) {
+      console.log(e)
+    })
+}
