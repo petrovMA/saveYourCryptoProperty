129ef9b27f8387ca74d5c3ff513aedcadeaf9334
petrov <petrovma92@gmail.com>
2019-04-30 22:32:33 +0300

    add alerts!!!

--- static/libs/tezos.js
+++ static/libs/tezos.js
@@ -1,7 +1,15 @@
+// main
+// let mnem = 'course casual original captain luggage husband knock october balance learn floor phone wheat bike snake'
+// let keys = eztz.crypto.generateKeys(mnem, 'iyuxlmea.qitsymvq@tezos.example.orgd0fe1QkS4V')
+// let keys = eztz.crypto.extractKeys('edsk3DNDUhzxh9vVZV4PN8KTTP9uLYRVqYedSDr12DuJmaS63cu7DV')
+
+// receiper tz1YWtADpgGehcTFqxMapiJcfLgzzL79aDJQ
+// let mnem = 'cruise pill rhythm enter remain flat sense traffic goat paper blossom super disagree mother speed'
+// let keys = eztz.crypto.generateKeys(mnem, 'gugmtvax.nuekgxlw@tezos.example.orgaEVLOPaRD3')
+// let keys = eztz.crypto.extractKeys('edsk4UAnyu14JfR4n7yW3aqr1mAEpWAn6WXiyaAFJnvCP1tCR1ibbA')
+
 function balance (keys) {
   eztz.node.setProvider('http://alphanet-node.tzscan.io')
-  // let mnem = 'course casual original captain luggage husband knock october balance learn floor phone wheat bike snake'
-  // let keys = eztz.crypto.generateKeys(mnem, 'iyuxlmea.qitsymvq@tezos.example.orgd0fe1QkS4V')
   let account = keys.pkh
   $('#account').html(account)
   eztz.rpc.getBalance(account).then(function (res) {
@@ -14,39 +22,67 @@ function balance (keys) {
 function pingContract (keys) {
   // eztz.node.setDebugMode(true)
   eztz.node.setProvider('https://alphanet-node.tzscan.io')
-
-  // let mnem = 'course casual original captain luggage husband knock october balance learn floor phone wheat bike snake'
-  // let keys000 = eztz.crypto.generateKeys(mnem, 'iyuxlmea.qitsymvq@tezos.example.orgd0fe1QkS4V')
-  // let keys = eztz.crypto.extractKeys('edsk3DNDUhzxh9vVZV4PN8KTTP9uLYRVqYedSDr12DuJmaS63cu7DV')
   let account = keys.pkh
-
   let addr = 'KT1REci1iEvCLJYYigY3TupRvBugRmdWNWTv'
   eztz.contract.send(addr, account, keys, 6, '(Right (Left Unit))', 100000, 400000, 60000)
     .then(function (res) {
-      console.log('Operation result!!!') // Operation result
-      console.log(res) // Operation result
-    }).catch(function (e) {
-      console.log(e)
+      success(res)
+    })
+    .catch(function (e) {
+      error(e)
     })
 }
 
 function receiveProperty (keys) {
   // eztz.node.setDebugMode(true)
   eztz.node.setProvider('https://alphanet-node.tzscan.io')
-
-  // let mnem = 'cruise pill rhythm enter remain flat sense traffic goat paper blossom super disagree mother speed'
-  // let keys = eztz.crypto.generateKeys(mnem, 'gugmtvax.nuekgxlw@tezos.example.orgaEVLOPaRD3')
   let account = keys.pkh
-
   let addr = 'KT1REci1iEvCLJYYigY3TupRvBugRmdWNWTv'
   // [{prim: "Right", args: [{prim: "Right", args: [{prim: "Left", args: [{prim: "Unit"}]}]}]}]
   eztz.contract.send(addr, account, keys, 6, '(Right (Right (Right (Left Unit))))', 100000, 400000, 60000)
     .then(function (res) {
-      console.log('Operation result!!!') // Operation result
-      console.log(res) // Operation result
-    }).catch(function (e) {
-      console.log(e)
+      success(res)
     })
+    .catch(function (e) {
+      error(e)
+    })
+}
+
+function success (res) {
+  try {
+    console.log(res)
+    let message = res.hash
+    $('#alerts').append('' +
+      '<div class="alert alert-success" id="success-notification">' +
+      '    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
+      '    <strong>Success!</strong>  Transaction: ' + message +
+      '</div>')
+  } catch {
+    $('#alerts').append('' +
+      '<div class="alert alert-info" id="error-notification">\n' +
+      '    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\n' +
+      '    <strong>Info!</strong>  Can\'t find sended transaction.' +
+      '</div>')
+  }
+}
+
+function error (e) {
+  console.log(e)
+  try {
+    let message = e.errors[1].with.args[0].string
+    console.log(e)
+    $('#alerts').append('' +
+      '<div class="alert alert-warning" id="warning-notification">' +
+      '    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
+      '    <strong>Warning!</strong>' + '  ' + message +
+      '</div>')
+  } catch {
+    $('#alerts').append('' +
+      '<div class="alert alert-danger" id="error-notification">\n' +
+      '    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\n' +
+      '    <strong>Error!</strong>  Can\'t send transaction.' +
+      '</div>')
+  }
 }
 
 function getKeysByPhrase (mnem, pass) {
